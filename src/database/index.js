0b69c790b09143463418aebb4aaf0715c373a377
Leonardo Antunes <leoantunesnunes@gmail.com>
2020-02-11 11:35:59 -0300

    alteração

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/taskapi', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+mongoose.connect('mongodb+srv://leooantunes:tasks123@cluster0-tlshy.mongodb.net/apitasks?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 mongoose.set('useCreateIndex', true);
 mongoose.Promise = global.Promise;
 
